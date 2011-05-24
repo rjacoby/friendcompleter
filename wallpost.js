3f63f1bead878828695b5875522a6d29a66c35ea
Rafi Jacoby <rafi.jacoby@razorfish.com>
2011-05-24 11:49:49 -0500

    changing prompt

--- wallpost.js
+++ wallpost.js
@@ -2,7 +2,7 @@ function toggleContent() {
   FB.getLoginStatus(function(response) {
     pickerDiv = $('#picker');
     if (response.session) {
-      pickerDiv.html("<h3>Type a friend\'s name to select them for wall post.</h3><div id='selector'><div id='selectorInput' style='display: inline-block; margin:10px;height:48px;'><input id='fbAutocomplete' size=50 style='font-size: 18px;' /></div><div id='postButton' display='none' style='display: none;'><a id='postButtonLink' href='#'>Post to Wall</a></div></div>");
+      pickerDiv.html("<span>Type a friend\'s name to select them for wall post.</span><div id='selector'><div id='selectorInput' style='display: inline-block; margin:10px;height:48px;'><input id='fbAutocomplete' size=50 style='font-size: 18px;' /></div><div id='postButton' display='none' style='display: none;'><a id='postButtonLink' href='#'>Post to Wall</a></div></div>");
       setupFriendCompleter("input#fbAutocomplete");
       
       $('input#fbAutocomplete').bind('autocompleteclose', function(event, ui) {
@@ -17,7 +17,7 @@ function toggleContent() {
       $('input#fbAutocomplete').focus();
 
     } else {
-      pickerDiv.html('<h3>Log in to try the friend picker.</div>');
+      pickerDiv.html('<span>Log in to try the friend picker.</div>');
     }
   });
 }
